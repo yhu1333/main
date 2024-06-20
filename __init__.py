ff9b36f08ab71c3cfe1d500dce5de2e4e1e84210
yhu1333 <145925810+yhu1333@users.noreply.github.com>
2024-06-20 14:37:43 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .controller import AndroidController
-from .hierarchy import UIHierarchy, Element, Event, Widget
-from .context import Activity
-from .util import center, parse_bound
-from .evaluator import MainEvaluator
-from .action import Action, ActionType, none_action, back_action, enter_action, restart_action, stop_action, click_action, swipe_action, text_action, longclick_action
